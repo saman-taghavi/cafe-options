30f93aee4f28c92541c9f471e9a95db031bb7b5b
saman-taghavi <taghavisni@gmail.com>
2026-08-05 18:13:54 +0330

    feat: restore AuthGate with 2127 passcode

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,6 +4,8 @@ import { TanStackDevtools } from '@tanstack/react-devtools'
 
 import appCss from '../styles.css?url'
 
+import { AuthGate } from '../components/AuthGate'
+
 export const Route = createRootRoute({
   head: () => ({
     meta: [
@@ -36,7 +38,9 @@ function RootDocument({ children }: { children: React.ReactNode }) {
         <script async src="//www.instagram.com/embed.js"></script>
       </head>
       <body>
-        {children}
+        <AuthGate>
+          {children}
+        </AuthGate>
         <TanStackDevtools
           config={{
             position: 'bottom-right',
