ea8d4bcc958981afc6fa79f83f3e4ecbef048852
saman-taghavi <taghavisni@gmail.com>
2026-08-05 23:17:48 +0330

    fix(embed): adjust permalink syntax and document title

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -17,7 +17,7 @@ export const Route = createRootRoute({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'TanStack Start Starter',
+        title: 'Where are we going today?',
       },
     ],
     links: [
